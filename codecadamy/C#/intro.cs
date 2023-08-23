90f4ecfcecfa326b3880325812efcea9b21061b6
JonFactor <Jon.Factor2@gmail.com>
2023-08-23 12:58:41 -0400

    epic

---
+++ codecadamy/C#/intro.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CodeCademySamples
+{
+    Class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wrld");
+
+            Console.WriteLine("your age?")
+            string input = Console.Readline();
+            Console.WriteLine($"You are {input} years old")
+        }
+    }
+}
