74fcf42db8806b6c634feb74874547b9f02a1650
JonFactor <Jon.Factor2@gmail.com>
2023-09-07 10:51:50 -0400

    432

--- 2023/VS/MajicEightBall/MajicEightBall/Program.cs
+++ 2023/VS/MajicEightBall/MajicEightBall/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace MajicEightBall
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Majic Eight Ball: Ask a quesiton...");
+			string userQuestion = Console.ReadLine();
+
+			Random ran = new Random();
+			int randomNumber = ran.Next(1, 9);
+
+			string answer = "";
+			switch (randomNumber)
+			{
+				case 1:
+					answer = "It is certain";
+					break;
+				case 2:
+					answer = "Reply hasty, try again.";
+					break;
+				case 3:
+			}
 		}
 	}
 }
